7847dd04b961afe9e2da422bcb41dab618b0b96a
Maykon Alves <maykon_man@yahoo.com.br>
2025-08-20 16:38:50 -0300

    Adicionar amigos na lista de amigo e exibir na tela a lista

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaAmigos = [];
+
+function adicionarAmigo(){
+    amigo = document.querySelector('input').value;
+    if(amigo=='' || amigo == null || amigo === undefined || !contemApenasLetrasOuEspaco(amigo)){
+        alert('Por favor, insira um nome válido');
+    }else{
+        if(listaAmigos.includes(amigo)){
+            alert('Amigo já cadastrado, cadastre outro amigo');
+        }else{
+            listaAmigos.push(amigo);
+            preencherCampo('listaAmigos',listaAmigos);
+        }
+    }
+    limparCampo();
+    
+}
+
+//Verificador que evita que digitem números no nome do amigo.
+function contemApenasLetrasOuEspaco(str) {
+  // A expressão regular /p{L} corresponde a qualquer letra de qualquer idioma (Unicode).
+  // O "u" no final é a flag de Unicode, necessária para o /p{L} funcionar.
+  // ^ e $ garantem que a string inteira, do início ao fim, corresponda ao padrão.
+  // O quantificador + significa "uma ou mais vezes".
+  const expressaoRegular = /^[\p{L} ]+$/u;
+
+  return expressaoRegular.test(str);
+}
+
+function limparCampo(){
+    amigo = document.querySelector('input');
+    amigo.value='';
+}
+function preencherCampo(id, texto){
+    campo = document.getElementById(id).innerHTML=texto;
+
+}
